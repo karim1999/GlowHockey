d6883cd0c9cfa680d8422e038ca6bbc8cdc7483e
karim <karim.elbadry2@gmail.com>
2019-07-31 21:16:28 +0200

    Network

--- GlowHockey/Program.cs
+++ GlowHockey/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Sockets;
+using System.IO;
 using System.Windows.Forms;
 
 namespace GlowHockey
@@ -7,7 +9,31 @@ namespace GlowHockey
     {
         public static void Main(string[] args)
         {
-            Application.Run(new GameForm());
+//            Application.Run(new GameForm());
+            TcpClient soc = new TcpClient("127.0.0.1", 6691);
+
+            StreamReader sr = new StreamReader(soc.GetStream());
+            StreamWriter sw = new StreamWriter(soc.GetStream());
+
+
+            //communicate with server
+            while (true)
+            {
+                Console.WriteLine("please enter a message:");
+                String userMsg = Console.ReadLine();
+                sw.WriteLine(userMsg);
+                sw.Flush();
+                String serverResponse = sr.ReadLine();
+
+                Console.WriteLine(serverResponse);
+                if (userMsg.Equals("bye"))
+                    break;
+            }
+
+
+            sr.Close();
+            sw.Close();
+            soc.Close();
         }
     }
 }
